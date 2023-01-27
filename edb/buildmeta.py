ShadiestGoat/edgedb

Add FTS functions and index.
Add the syntax for extending indexes.
Add the syntax for parametrizing indexes.

Add `fts` module with an index duitable for full-text search as well as
accompanying search functions.

fts::test - boolean test for FTS search
fts::match_rank - return the match rank
fts::highlight_match - return the matching part

edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2022_11_28_00_00
+EDGEDB_CATALOG_VERSION = 2023_01_13_00_00
 EDGEDB_MAJOR_VERSION = 3
 
 

rohankumardubey/edgedb-core

edb/buildmeta.py
@@ -44,7 +44,7 @@ from edb.common import verutils
 
 
 # Increment this whenever the database layout or stdlib changes.
-EDGEDB_CATALOG_VERSION = 2022_11_28_00_00
+EDGEDB_CATALOG_VERSION = 2023_01_27_00_00
 EDGEDB_MAJOR_VERSION = 3
 
 